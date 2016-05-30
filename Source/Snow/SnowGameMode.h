ce9a4f071c1ed1334754fac2265efaa11d7366d0
Jeremy Bernstein <utilityjerm@gmail.com>
2016-05-30 11:27:35 -0700

    Initial commit

---
+++ Source/Snow/SnowGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SnowGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNOW_API ASnowGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
