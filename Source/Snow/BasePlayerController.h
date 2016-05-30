764e8b8883d7720a1795c4e72a352aebe5c90a93
Jeremy Bernstein <utilityjerm@gmail.com>
2016-05-30 13:36:42 -0700

    Added test network code for head replication

---
+++ Source/Snow/BasePlayerController.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "BasePlayerController.generated.h"
+
+UCLASS()
+class SNOW_API ABasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ABasePlayerController();
+
+};
